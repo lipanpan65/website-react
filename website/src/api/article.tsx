871cc0fd989f233985cec299139539c1c38f591d
lipanpan65 <61178683+lipanpan65@users.noreply.github.com>
2024-04-01 18:57:47 +0800

    feat: 添加文章的管理API

---
+++ website/src/api/article.tsx
@@ -0,0 +1,16 @@
+
+import request from '@/utils/request'
+
+
+export default {
+    getArticleList:(params) => {
+
+    },
+    createArticle:(data) => {
+    },
+    updateArticle:(data) => {
+
+    },
+    publist
+}
+
---
+++ website/src/api/index.tsx
@@ -0,0 +1,5 @@
+
+// api/index.js
+
+export * from './userApi';
+export * from './postApi';
